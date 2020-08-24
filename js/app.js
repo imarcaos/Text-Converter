0e12a0f63b74e2b8044194189b79d050d477f357
Marcos Melo <marcaos.melo@gmail.com>
2020-08-24 19:50:27 +0100

    Update textarea Input and Output Function
    atualização na função de entrada e saída JS para a textarea

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-window.alert("Start of Text Converter");
+/*
+    App Text Converter
+    By eMarcaos 
+    Start Date: 22/08/2020
+    Version: 0.1-8.20
+*/
+
+function outputText(textOut) {
+    document.getElementById('textInOut').value = textOut;
+    console.log("Texto de Saída " + textOut);    
+}
+
+function upCase() {
+    //Converte o texto para todas as letras maiúsculas
+    let textIn = "";
+    textIn = document.getElementById("textInOut").value.toUpperCase();
+    //console.log("Função upCase " + textIn);
+    outputText(textIn);
+}

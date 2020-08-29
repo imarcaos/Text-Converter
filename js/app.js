1e0efc2179b414ecd901d3aa7793dfc7c15a9c2e
Marcos Melo <marcaos.melo@gmail.com>
2020-08-29 23:21:28 +0100

    Update app.js
    array

--- js/app.js
+++ js/app.js
@@ -24,15 +24,56 @@ function lCase() { //Converte o texto para todas as letras minúsculas
 }
 
 function uCaseFirstParaghWord() { //Converte para maiúscula a primeira letra do paragráfo
+    let textIn = "", nW = ""; // var text Input e new Word
+    textIn = document.getElementById("textInOut").value.toLowerCase();
+    nW = textIn.split(" ");    
+    console.log(nW);
+    // esta com erro quando texto inicia com espaço em branco (inicio da array) (29/08/2020)
+    // não converte para maiúscula quando o ponto está na primeira array
+    //nW = nW.splice(" ",nW.indexOf(" ") );
+    for (let i = 0; i < nW.length; i++) {        
+
+        if ( i == 0 ){
+            let w = nW[i];
+            let firstLetter = w[0];
+            w = firstLetter.toUpperCase() + w.slice(1);
+            nW[i] = w;
+        } else if ( nW[i].indexOf(".") >= 0 ){
+            let w = nW[i+1];
+            let firstLetter = w[0];
+            w = firstLetter.toUpperCase() + w.slice(1);
+            nW[i+1] = w;
+        }
+    }
+    console.log(nW.join(" "));
+}
+
+function ex_uCaseFirstParaghWord() { // fica como exemplo
     let textIn = "", nW = "";
     textIn = document.getElementById("textInOut").value.toLowerCase();
-    //console.log(textIn);
+    nW = textIn.split(" ");
+    for (var a = 0; a < nW.length; a++) {
+        var w = nW[a];
 
-    for (let i = 0; i < textIn.length; i++) {        
-        console.log(textIn[i]);
+        var firstLetter = w[0];
+        w = firstLetter.toUpperCase() + w.slice(1);
 
-        if (textIn.indexOf(" ") > i) {
-            nW = textIn[i].toUpperCase();
+        nW[a] = w;
+    }
+    console.log(nW.join(" "));
+    //return nW.join(" ");
+}
+
+
+function auCaseFirstParaghWord() { //Converte para maiúscula a primeira letra do paragráfo
+    let textIn = "", nW = "";
+    textIn = document.getElementById("textInOut").value.toLowerCase();
+    console.log(textIn && textIn[0].toUpperCase() + textIn.slice(1)); //nao funciona com primeiro espaço em branco
+
+    for (let i = 0; i < textIn.length; i++) {       
+
+        if (textIn.indexOf(" ") == i) {
+            nW = textIn[i]
             console.log("-> " + i + " " + nW);
         }
         

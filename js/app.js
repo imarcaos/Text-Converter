f64929a968ad1f41c6b3bdec23ff885fb0edadfe
Marcos Melo <marcaos.melo@gmail.com>
2020-08-31 22:22:04 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -27,69 +27,39 @@ function uCaseFirstParaghWord() { //Converte para maiúscula a primeira letra do
     let textIn = "", nW = ""; // var text Input e new Word
     textIn = document.getElementById("textInOut").value.toLowerCase();
     nW = textIn.split(" ");    
-    console.log(nW);
+    //console.log(nW);
     // esta com erro quando texto inicia com espaço em branco (inicio da array) (29/08/2020)
-    // não converte para maiúscula quando o ponto está na primeira array
-    //nW = nW.splice(" ",nW.indexOf(" ") );
+    // erro com mais de um espaço em branco entre palavras (31/08/2020)
+    // erro quando possui apenas um palavra e ponto final (31/08/2020)
+    
     for (let i = 0; i < nW.length; i++) {        
 
-        if ( i == 0 ){
-            let w = nW[i];
-            let firstLetter = w[0];
-            w = firstLetter.toUpperCase() + w.slice(1);
-            nW[i] = w;
-        } else if ( nW[i].indexOf(".") >= 0 ){
+        if ( i == 0 && nW[i] != " "){
+            if (  nW[i].indexOf(".") >= 0 ) {
+                let w = nW[i];
+                let firstLetter = w[0];
+                w = firstLetter.toUpperCase() + w.slice(1);
+                nW[i] = w;
+                let w1 = nW[i+1];
+                let firstLetter1 = w1[0];
+                w1 = firstLetter1.toUpperCase() + w1.slice(1);
+                nW[i+1] = w1;
+            } else {
+                nW[i] == nW[i+1];
+                /* let w = nW[i];
+                let firstLetter = w[0];
+                w = firstLetter.toUpperCase() + w.slice(1);
+                nW[i] = w; */
+            }
+            
+        } else if ( nW[i].indexOf(".") >= 0 && nW.length < i ){
             let w = nW[i+1];
             let firstLetter = w[0];
             w = firstLetter.toUpperCase() + w.slice(1);
             nW[i+1] = w;
+        } else if (nW == " ") {
+            console.log("É um espaço ->" + nW[i]);
         }
     }
     console.log(nW.join(" "));
-}
-
-function ex_uCaseFirstParaghWord() { // fica como exemplo
-    let textIn = "", nW = "";
-    textIn = document.getElementById("textInOut").value.toLowerCase();
-    nW = textIn.split(" ");
-    for (var a = 0; a < nW.length; a++) {
-        var w = nW[a];
-
-        var firstLetter = w[0];
-        w = firstLetter.toUpperCase() + w.slice(1);
-
-        nW[a] = w;
-    }
-    console.log(nW.join(" "));
-    //return nW.join(" ");
-}
-
-
-function auCaseFirstParaghWord() { //Converte para maiúscula a primeira letra do paragráfo
-    let textIn = "", nW = "";
-    textIn = document.getElementById("textInOut").value.toLowerCase();
-    console.log(textIn && textIn[0].toUpperCase() + textIn.slice(1)); //nao funciona com primeiro espaço em branco
-
-    for (let i = 0; i < textIn.length; i++) {       
-
-        if (textIn.indexOf(" ") == i) {
-            nW = textIn[i]
-            console.log("-> " + i + " " + nW);
-        }
-        
-        /*
-        if ( i == 0) {
-            //nW = textIn[i].toLowerCase().replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
-            //console.log(nW);
-
-        } else if (nW[i] ===  ".") {
-            //nW += nW.toLowerCase().replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
-            //console.log(nW);
-
-        }
-        */
-            
-    }
-    
-    //console.log('xxx1,xxx'.search('1\\,'));
 }

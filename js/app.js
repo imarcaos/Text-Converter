d98f5c7323ed11d8f3ab034407885f174ff0374c
Marcos Melo <marcaos.melo@gmail.com>
2020-09-18 23:25:58 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -74,24 +74,26 @@ function uCaseFirstParaghWord() { //Converte para maiúscula a primeira letra do
                 nW[i] = w;
             }
             
-        } else if ( nW[i].indexOf(".") >= 0 ) {  //falha: ponto final espaço e enter e último ponto final
-            console.log("ultimo else");
+        } else if ( nW[i].indexOf(".") >= 0 ) {
 
-            if ( verifyChar(nW[i]) !== 0 && verifyChar(nW[i]) != nW[i].length ) {
+            if ( verifyChar(nW[i]) !== 0 && verifyChar(nW[i]) != nW[i].length ) { // falha: aumentar tamanho letra a seguir ao ponto, mesma linha.
+                
                 let pChar = verifyChar(nW[i]);
                 let wd1 = nW[i].slice(0, pChar);
                 let wd2 = nW[i].slice(pChar+1);
                 let nextLetter = nW[i][pChar];
                 nW[i] = wd1 + nextLetter.toUpperCase() + wd2;
                 console.log(verifyChar(nW[i]));
-            } else if ( verifyChar(nW[i+1]) !== 0 ) {
+
+            } /* else if ( verifyChar(nW[i+1]) !== 0 && verifyChar(nW[i+1]) != nW[i+1].length ) { // erro: ponto final na ultima palavra e com enter no final sem palavras a seguir
+                console.log("if 2");
                 let pChar = verifyChar(nW[i+1]);
                 let wd1 = nW[i+1].slice(0, pChar);
                 let wd2 = nW[i+1].slice(pChar+1);
                 let nextLetter = nW[i+1][pChar];
                 nW[i+1] = wd1 + nextLetter.toUpperCase() + wd2;
                 console.log(verifyChar(nW[i+1]));
-            }
+            } */
 
         }
 
